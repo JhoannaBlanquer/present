394f43ad4817cdf1d87788727574aab38c523d02
NishaLi24 <nishationgzon2003@gmail.com>
2025-01-30 17:05:00 +0800

    Revert "firebase"
    This reverts commit 918bddb1fcaa4bd3ed3ba638409ed2cd12c8e18a.

--- present/src/app/app.module.ts
+++ present/src/app/app.module.ts
@@ -16,6 +16,15 @@ import { ActivityComponent } from './activity/activity.component';
 import { CreateclassComponent } from './createclass/createclass.component';
 import { JoinclassComponent } from './joinclass/joinclass.component';
 import { CheckinconnectComponent } from './checkinconnect/checkinconnect.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { initializeAppCheck, ReCaptchaEnterpriseProvider, provideAppCheck } from '@angular/fire/app-check';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from '../environments/environment.development';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -37,9 +46,25 @@ import { CheckinconnectComponent } from './checkinconnect/checkinconnect.compone
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    provideFirebaseApp(() => initializeApp({ projectId: "present-db-83b6e", appId: "1:970574739382:web:8ed00089a54ecb0379f317", storageBucket: "present-db-83b6e.firebasestorage.app", apiKey: "AIzaSyDDwbhnR_96x4tQ6cqhNgZiIeQj_66MGeY", authDomain: "present-db-83b6e.firebaseapp.com", messagingSenderId: "970574739382" })),
+    provideAuth(() => getAuth()),
+    provideAnalytics(() => getAnalytics()),
+    ScreenTrackingService,
+    UserTrackingService,
+
+    /* provideAppCheck(() => {
+      // TODO get a reCAPTCHA Enterprise here https://console.cloud.google.com/security/recaptcha?project=_
+      const provider = new ReCaptchaEnterpriseProvider(reCAPTCHA Enterprise site key );
+      return initializeAppCheck(undefined, { provider, isTokenAutoRefreshEnabled: true });
+    }),
+  */
+    provideFirestore(() => getFirestore())
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- present/src/app/sign-up/sign-up.component.ts
+++ present/src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../shared/auth.service';
+
 
 @Component({
   selector: 'app-sign-up',
@@ -7,6 +9,31 @@ import { Component } from '@angular/core';
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
-export class SignUpComponent {
+export class SignUpComponent implements OnInit{
 
+  email : string = '';
+  password : string = '';
+
+  constructor(private auth : AuthService) {}
+
+  ngOnInit(): void {
+    
+  }
+
+  register() {
+    if(this.email == '') {
+      alert('Please enter email');
+    return;
+    }
+
+    if(this.password == '') {
+      alert('Please enter password');
+    return;
+    }
+
+    this.auth.register(this.email,this.password);
+    
+    this.email;
+    this.password;
+  }
 }
